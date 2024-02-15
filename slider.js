8f8f1be0a9e86f2b96d68ef0048517b98b7f0164
viveksai-11 <128347626+viveksai-11@users.noreply.github.com>
2024-02-15 11:21:22 +0530

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,24 @@
+// slider.js
+
+
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    responsive:{
+      0:{
+        items:1
+      },
+      600:{
+        items:2
+      },
+      1000:{
+        items:3
+      }
+    }
+  });
+});
